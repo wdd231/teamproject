97c63f7b5d5d08cbccbf74484a2cdd8dd3129c1d
dannykim1837 <eoghks1837@gmail.com>
2025-02-27 14:57:13 -0700

    main1

--- team/script.js
+++ team/script.js
@@ -1,19 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const toggleBtn = document.querySelector(".navbar_toggleBtn");
-    const menu = document.querySelector(".navbar_menu");
-    const icons = document.querySelector(".navbar_icons");
-
-    toggleBtn.addEventListener("click", function () {
-        menu.classList.toggle("active");
-        icons.classList.toggle("active");
-    });
-
-    // dropdown menu (for mobile)
     const dropdown = document.querySelector(".dropdown");
     const dropdownMenu = document.querySelector(".dropdown_menu");
 
     dropdown.addEventListener("click", function (event) {
-        event.preventDefault();
-        dropdownMenu.classList.toggle("active");
+        event.preventDefault(); 
+        dropdownMenu.classList.toggle("active"); 
+    });
+
+  
+    document.addEventListener("click", function (event) {
+        if (!dropdown.contains(event.target)) {
+            dropdownMenu.classList.remove("active");
+        }
     });
 });
